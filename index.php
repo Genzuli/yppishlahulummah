0780c7d82960629f98460c9a99cb6904fcdd3460
Genzuli <165475818+Genzuli@users.noreply.github.com>
2024-06-23 23:43:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,604 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>YAYASAN PONPES ISHLAHUL UMMAH NW BUNUT BAOK</title>
+		<link href="img/logo-ponpes.png" rel="icon">
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+		<link rel="stylesheet" href="css/style2.css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+    </head>
+	<body>
+
+		<!-- Header -->
+		<header id="header" class="transparent-nav">
+			<div class="container">
+
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a class="logo" href="index.php">
+							<img src="./img/logo2.png" alt="logo">
+						</a>
+					</div>
+					<!-- /Logo -->
+
+					<!-- Mobile toggle -->
+					<button class="navbar-toggle">
+						<span></span>
+					</button>
+					<!-- /Mobile toggle -->
+				</div>
+
+				<!-- Navigation -->
+				<nav id="nav">
+					<ul class="main-menu nav navbar-nav navbar-right">
+						<li><a href="index.php">Beranda</a></li>
+						<li><a href="#about">Tentang</a></li>
+						<li><a href="#why-us">Lembaga Pendidikan</a></li>
+						<li><a href="#courses">Program Unggulan</a></li>
+						<li><a href="contact.php">Kontak</a></li>
+					</ul>
+				</nav>
+				<!-- /Navigation -->
+
+			</div>
+		</header>
+		<!-- /Header -->
+
+		<!-- Home -->
+		<div id="home" class="hero-area">
+
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/latarbelakang.jpg)"></div>
+			<!-- /Backgound Image -->
+
+			<div class="home-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8">
+							<h1 class="white-text">Yayasan Ponpes Ishlahul Ummah NW Bunut Baok</h1>
+							<p class="lead white-text">Mewujudkan generasi yang berakhlak mulia dan berprestasi berdasarkan Iman dan Takwa</p>
+							<a class="main-button icon-button" href="contact.php">Mulai</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- /Home -->
+
+		<!-- Tentang -->
+		<div id="about" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<div class="col-md-8">
+						<div class="section-header">
+							<h3>TENTANG</h3>
+						</div>
+
+						<!-- feature -->
+						<div class="feature">
+							<i class="feature-icon fa fa-file"></i>
+							<div class="feature-content">
+								<h4>Sejarah </h4>
+								<p>Ceteros fuisset mei no, soleat epicurei adipiscing ne vis. Et his suas veniam nominati.</p>
+							</div>
+						</div>
+						<!-- /feature -->
+
+						<!-- feature -->
+						<div class="feature">
+							<i class="feature-icon fa fa-flask"></i>
+							<div class="feature-content">
+								<h4>Visi</h4>
+								<p>Ceteros fuisset mei no, soleat epicurei adipiscing ne vis. Et his suas veniam nominati.</p>
+							</div>
+						</div>
+						<!-- /feature -->
+
+						<!-- feature -->
+						<div class="feature">
+							<i class="feature-icon fa fa-flask"></i>
+							<div class="feature-content">
+								<h4>Misi</h4>
+								<p>Ceteros fuisset mei no, soleat epicurei adipiscing ne vis. Et his suas veniam nominati.</p>
+							</div>
+						</div>
+						<!-- /feature -->
+
+					</div>
+
+					<div class="col-md-4">
+						<div class="about-img">
+							<img src="./img/pendiri1.svg" alt="">
+						</div>
+					</div>
+
+				</div>
+				<!-- row -->
+
+			</div>
+			<!-- container -->
+		</div>
+		<!-- /Tentang -->
+		 <!-- Why us -->
+		<div id="why-us" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+					<div class="section-header text-center">
+						<h2>LEMBAGA PENDIDIKAN</h2>
+						<p class="lead">Yayasan Ponpes Ishlahul Ummah menaungi 4 Lembaga Pendikan</p>
+					</div>
+
+					<!-- feature -->
+					<div class="col-md-3">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>MADRASAH IBTIDAIYAH NW BALOK TUI</h4>
+								<p>Sekolah ini Sederajat dengan Sekolah Dasar(SD). Sekolah ini sendiri berada di Dusun Tui, Desa Dadap, Kec. Pujut</p>
+								<h5>Non Asrama</h5>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+					<!-- feature -->
+					<div class="col-md-3">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>MTs ISHLAHUL UMMAH NW BUNUT BAOK</h4>
+								<p>Sekolah ini Sederajat dengan SMP. Sekolah ini sendiri berada di Dusun Bunut Baok, Desa Dadap, Kec. 
+									Pujut. Sekaligus berseblahan dengan Gedung Yayasan. </p>
+									<h5>Non Asrama</h5>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+					<!-- feature -->
+					<div class="col-md-3">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>SALFIYAH WUSTHO ISHLAHUL UMMAH NW BUNUT BAOK</h4>
+								<p>Sekolah ini Sederajat dengan SMP. Sekolah ini sendiri berada di Dusun Bunut Baok, Desa Dadap, Kec. 
+									Pujut. Sekaligus berseblahan dengan Gedung Yayasan. </p>
+									<h5>Wajib Asrama</h5>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+					<!-- feature -->
+					<div class="col-md-3">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>MA ISHLAHUL UMMAH NW BUNUT BAOK</h4>
+								<p>Sekolah ini Sederajat dengan SMA/SMK. Sekolah ini sendiri berada di Dusun Bunut Baok, Desa Dadap, Kec. 
+								Pujut. Sekaligus berseblahan dengan Gedung Yayasan.</p>
+								<h5>Wajib Asrama</h5>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+				</div>
+
+
+			</div>
+			<!-- /container -->
+
+		</div>
+		<!-- /Why us -->
+
+	
+
+		<!-- Courses -->
+		<div id="courses" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+					<div class="section-header text-center">
+						<h2>PROGRAM UNGGULAN</h2>
+						<p class="lead">Yayasan Pondok Pesantren Ishlahul Ummah juga memiliki beberapa Program Unggulan yang bertujuan untuk mengasah kemampuan Santri Santriwatinya</p>
+					</div>
+				</div>
+				<!-- /row -->
+
+				<!-- courses -->
+				<div id="courses-wrapper">
+
+					<!-- row -->
+					<div class="row">
+
+						<!-- single course -->
+						<div class="col-md-3 col-sm-6 col-xs-6">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program4.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>BIMBINGAN MEMBACA KITAB KUNING</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+						<!-- single course -->
+						<div class="col-md-3 col-sm-6 col-xs-6">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program5.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>BAHASA INGGRIS</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+						<!-- single course -->
+						<div class="col-md-3 col-sm-6 col-xs-6">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program6.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>BAHASA ARAB</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+						<!-- single course -->
+						<div class="col-md-3 col-sm-6 col-xs-6">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program7.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>NAHWU SHOROF</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+						<!-- single course -->
+						<div class="col-md-3 col-sm-6 col-xs-6">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program2.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>TILAWATIL QUR'AN</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+						<!-- single course -->
+						<div class="col-md-3 col-sm-6 col-xs-6">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program1.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>HAFIDZ AL-QUR'AN</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+						<!-- single course -->
+						<div class="col-md-3 col-sm-5 col-xs-5">
+							<div class="course">
+								<a href="#" class="course-img">
+									<img src="./img/program3.jpg" alt="">
+									<i class="course-link-icon fa fa-link"></i>
+								</a>
+								<h5>HIFZUL MATAN</h5>
+								<div class="course-details">
+									<span class="course-price course-premium">Wajib Asrama</span>
+								</div>
+							</div>
+						</div>
+						<!-- /single course -->
+
+						
+
+					</div>
+					<!-- /row -->
+
+				</div>
+				<!-- /courses -->
+
+				<div class="row">
+					<div class="center-btn">
+						<a class="main-button icon-button" href="#">Selengkapnya</a>
+					</div>
+				</div>
+
+			</div>
+			<!-- container -->
+
+		</div>
+		<!-- /Courses -->
+
+		<!-- Call To Action -->
+		<div id="cta" class="section">
+
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/latarbelakang2.jpg)"></div>
+			<!-- /Backgound Image -->
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<div class="col-md-6">
+						<h2 class="white-text">Segera Daftarkan Putra Putri Anda!</h2>
+						<p class="lead white-text">Untuk mendaftarkan Putra Putri anda bisa langsung ke Yayasan, atau dengan mengklik tombol dibawah</p>
+						<a class="main-button icon-button" href="#">Daftar</a>
+					</div>
+
+				</div>
+				<!-- /row -->
+
+			</div>
+			<!-- /container -->
+
+		</div>
+		<!-- /Call To Action -->
+
+		<!-- Why us -->
+		<div id="why-us" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+					<div class="section-header text-center">
+						<h2>FASILITAS</h2>
+						<p class="lead">Berikut Fasilitas yang disediakan oleh Yayasan</p>
+					</div>
+					<div class="section-header text-center">
+						<h3>Ekstra Kurikuler</h3>
+					</div>
+
+					<!-- feature -->
+					<div class="col-md-4">
+						<div class="feature">
+							<i class="feature-icon fa fa-users"></i>
+							<div class="feature-content">
+								<h4>HADROH</h4>
+								<p>Hadroh adalah kesenian Islam yang melibatkan permainan alat musik rebana atau gendang, diiringi nyanyian shalawat</p>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+					<!-- feature -->
+					<div class="col-md-4">
+						<div class="feature">
+							<i class="feature-icon fa fa-users"></i>
+							<div class="feature-content">
+								<h4>KASIDAH</h4>
+								<p>
+								Kasidah adalah bentuk puisi atau syair dalam tradisi sastra Arab yang biasanya berisi pujian, nasihat, atau petuah.</p>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+					<!-- feature -->
+					<div class="col-md-4">
+						<div class="feature">
+							<i class="feature-icon fa fa-users"></i>
+							<div class="feature-content">
+								<h4>KALIGRAFI</h4>
+								<p>
+								Kaligrafi adalah seni menulis atau menggambar huruf dengan indah dan estetis, yang menjadi bagian penting dari seni Islam.</p>
+							</div>
+						</div>
+					</div>
+					<br>
+					
+				<hr class="section-hr">
+					<!-- /feature -->
+					<div class="section-header text-center">
+						<h3>Gedung</h3>
+					</div>
+
+					<!-- feature -->
+					<div class="col-md-4">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>Madrasah Aliyah</h4>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+					<!-- feature -->
+					<div class="col-md-4">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>Asrama Putri</h4>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+					<!-- feature -->
+					<div class="col-md-4">
+						<div class="feature">
+							<i class="feature-icon fa fa-home"></i>
+							<div class="feature-content">
+								<h4>Asrama Putra</h4>
+							</div>
+						</div>
+					</div>
+					<!-- /feature -->
+
+				</div>
+				<!-- /row -->
+				<hr class="section-hr">
+			</div>
+			<!-- /container -->
+
+		</div>
+		<!-- /Why us -->
+
+		<!-- Contact CTA -->
+		<div id="contact-cta" class="section">
+
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/cta2-background.jpg)"></div>
+			<!-- Backgound Image -->
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<div class="col-md-8 col-md-offset-2 text-center">
+						<h2 class="white-text">Hubungi Kami</h2>
+						<p class="lead white-text">Jika anda merasa kekurangan Informasi, bisa langsung tanyakan melalui tombol dibawah!</p>
+						<a class="main-button icon-button" href="contact.php">Kontak</a>
+					</div>
+
+				</div>
+				<!-- /row -->
+
+			</div>
+			<!-- /container -->
+
+		</div>
+		<!-- /Contact CTA -->
+
+		<!-- Footer -->
+		<footer id="footer" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+
+					<!-- footer logo -->
+					<div class="col-md-4">
+						<div class="footer-logo">
+							<a class="logo" href="index.php">
+								<img src="./img/logo2.png" alt="logo">
+							</a>
+						</div>
+					</div>
+					<!-- footer logo -->
+
+					<!-- footer nav -->
+					<div class="col-md-8">
+						<ul class="footer-nav">
+							<li><a href="index.php">Beranda</a></li>
+							<li><a href="#">Tentang</a></li>
+							<li><a href="#">Lembaga Pendidikan</a></li>
+							<li><a href="blog.php">Program Unggulan</a></li>
+							<li><a href="contact.php">Kontak>
+						</ul>
+					</div>
+					<!-- /footer nav -->
+
+				</div>
+				<!-- /row -->
+
+				<!-- row -->
+				<div id="bottom-footer" class="row">
+
+					<!-- social -->
+					<div class="col-md-4 col-md-push-8">
+						<ul class="footer-social">
+							<li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+							<li><a href="#" class="youtube"><i class="fa fa-youtube"></i></a></li>
+						</ul>
+					</div>
+					<!-- /social -->
+
+					<!-- copyright -->
+					<div class="col-md-8 col-md-pull-4">
+					<div class="footer-copyright">
+							<span>&copy; Copyright 2024. All Rights Reserved. | YPP Ishlahul Ummah NW Bunut Baok</span>
+						</div>
+					</div>
+					<!-- /copyright -->
+
+				</div>
+				<!-- row -->
+
+			</div>
+			<!-- /container -->
+
+		</footer>
+		<!-- /Footer -->
+
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+		<!-- /preloader -->
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</body>
+</html>
